84edf87d2a5afdfcd44eaf7bf9ebdccce2648c64
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-03-12 16:47:53 +0200

    initial commit: created functn to test if string is number, conditional statements for results

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+    let input =/* "xrdtfcvubhknj645o';"*/document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let Code = "pet_"
+    let isCode = input.slice(0,4)  
+    let result =  // Placeholder for validation result
+    booli =false
+    let isYear = input.slice(5,8)
 
+
+    function isNumber(isYear) {
+        return !isNaN(parseInt(isYear)) && isFinite(isYear);                
+        }
+
+
+    if(Code===isCode && isNumber(isYear)===true)
+    {booli = true}
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
+        if(booli===true)
+        {result= "Valid Index" }
+        else if (input==="")
+        { result=""}
+        else {result="Invalid Index 🛑"}
+           document.getElementById('result').innerText = result;
 }
 
 
+console.log(validateSyntax())
