5d65118eb581a9243ae86061a36526015ecd51c4
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-03-13 16:55:19 +0200

    comments added

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation 
     let Code = "pet_"
-    let isCode = input.slice(0,4)                                           //chop the code-word
+    let isCode = input.slice(0,4)                  //chop the code-word
     let result = ""
     booli =false
-    let isYear = input.slice(5,8)                                           //chop the date section
+    let isYear = input.slice(5,8)              //chop the date section
     let isName = input.slice(9,100)     //chop the name section
                                     
 
 
     //these are for determining if last bit is text
     function alphabetTester(){
-    let regex = /^[a-zA-Z]+$/;
+    let regex = /^[a-zA-Z]+$/;                    // tests string for alphabets, brings true isf alphabets only else false
     return regex.test(isName)  
     }
 
     //test if index 5-8 are numerical entry
     function isNumber(isYear) {
-        return !isNaN(parseInt(isYear)) && isFinite(isYear);                
+        return !isNaN(parseInt(isYear)) && isFinite(isYear);           // tests string if it is a numerical value,      
         }
         
 
